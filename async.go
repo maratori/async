607b7de60efc4eb9480d5967cfde8aae767012c8
Marat Reymers <marat.reymers@gett.com>
2022-07-27 09:08:50 +0300

    Implement `Executor`

--- async.go
+++ async.go
@@ -1,1 +1,292 @@
 package async
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"reflect"
+	"runtime"
+	"time"
+
+	"github.com/pkg/errors"
+	"github.com/shopspring/decimal"
+)
+
+type methodID struct {
+	rType reflect.Type
+	name  string
+}
+
+type methodInfo struct {
+	jobName           string
+	receivesContext   bool
+	returnsError      bool
+	marshalStructType reflect.Type
+	paramTypes        []string
+}
+
+type Executor struct {
+	methods map[methodID]methodInfo
+}
+
+func NewExecutor() *Executor {
+	return &Executor{
+		methods: map[methodID]methodInfo{},
+	}
+}
+
+// Register returns a handler to call a method on the receiver.
+// The handler should be registered in jobs processor.
+//
+// Method is a func that takes receiver as the first input parameter.
+// The next input parameter may optionally be context.Context.
+// It optionally may return an error (or a type implementing error interface).
+//
+// Receiver should have the same type as the first parameter of the method.
+// I.e. if method is declared on a pointer, receiver should be a pointer.
+//
+// You can't register the same jobName twice.
+// You can't register the same method twice.
+func (e *Executor) Register( // nolint:gocognit // TODO: simplify
+	jobName string,
+	receiver interface{},
+	method interface{},
+) (func(context.Context, json.RawMessage) error, error) {
+	receiverVal := reflect.ValueOf(receiver)
+	methodVal := reflect.ValueOf(method)
+	methodType := methodVal.Type()
+
+	info, err := e.validateAndGetMethodInfo(jobName, receiverVal.Type(), methodType)
+	if err != nil {
+		return nil, errors.WithMessagef(err, "invalid method")
+	}
+
+	id := e.methodID(methodVal)
+
+	if _, ok := e.methods[id]; ok {
+		return nil, errors.Errorf("method already registered %s %s", id.name, methodType)
+	}
+	for _, i := range e.methods {
+		if jobName == i.jobName {
+			return nil, errors.Errorf("jobName already registered %q", jobName)
+		}
+	}
+
+	e.methods[id] = info
+
+	return func(ctx context.Context, marshaledArgs json.RawMessage) error {
+		marshalStructPtr := reflect.New(info.marshalStructType)
+		marshalStruct := marshalStructPtr.Elem()
+
+		errJSON := json.Unmarshal(marshaledArgs, marshalStructPtr.Interface())
+		if errJSON != nil {
+			return errors.Wrapf(errJSON, "can't unmarshal arguments")
+		}
+
+		args := make([]reflect.Value, 0, methodType.NumIn())
+		args = append(args, receiverVal)
+		if info.receivesContext {
+			args = append(args, reflect.ValueOf(ctx))
+		}
+
+		for i, expectedType := range info.paramTypes {
+			actualType := marshalStruct.Field(1 + i*2).String()
+			if actualType == "" {
+				return errors.Errorf("args[%d]: missing value (type %s)", i, expectedType)
+			}
+			if expectedType != actualType {
+				return errors.Errorf("args[%d]: wrong type: %s expected, received %s", i, expectedType, actualType)
+			}
+
+			args = append(args, marshalStruct.Field(i*2)) // nolint:gomnd // even numbers
+		}
+
+		res := methodVal.Call(args)
+
+		if !info.returnsError {
+			return nil
+		}
+		if res[0].IsNil() {
+			return nil
+		}
+		return (res[0].Interface()).(error)
+	}, nil
+}
+
+// Prepare returns jobName and marshaled args to be enqueued.
+//
+// Arguments are serialized to be saved inside jobs storage.
+// It is a bad practice to use domain types as a transport ones directly.
+// So library limits allowed types as much as possible.
+// However, it still may be error-prone, see test "using iota is dangerous".
+func (e *Executor) Prepare(method interface{}, args ...interface{}) (string, json.RawMessage, error) {
+	methodType := reflect.TypeOf(method)
+	if methodType.Kind() != reflect.Func {
+		return "", nil, errors.Errorf("func expected, received %s", methodType)
+	}
+
+	id := e.methodID(reflect.ValueOf(method))
+
+	info, ok := e.methods[id]
+	if !ok {
+		return "", nil, errors.Errorf("not registered method %s %s", id.name, methodType)
+	}
+
+	expectedLen := len(info.paramTypes)
+	if len(args) != expectedLen {
+		return "", nil, errors.Errorf("expected %d args, received %d", expectedLen, len(args))
+	}
+
+	marshalStructPtr := reflect.New(info.marshalStructType)
+	marshalStruct := marshalStructPtr.Elem()
+	offset := methodType.NumIn() - expectedLen
+
+	for i := 0; i < expectedLen; i++ {
+		eType := methodType.In(offset + i)
+		aType := reflect.TypeOf(args[i])
+		// TODO: [good idea?] convert nil to zero value of expected type
+		//                    type MyTypeWithIgnoredValue string
+		// TODO: [good idea?] compare kinds and do conversion if matches (may be useful if type MyInt int is used, but gust )
+		if aType != eType {
+			return "", nil, errors.Errorf("args[%d]: expected %s, received %s", i, eType, aType)
+		}
+
+		marshalStruct.Field(i * 2).Set(reflect.ValueOf(args[i])) // nolint:gomnd // even numbers
+		marshalStruct.Field(1 + i*2).SetString(info.paramTypes[i])
+	}
+
+	data, err := json.Marshal(marshalStructPtr.Interface())
+	if err != nil {
+		return "", nil, errors.Wrapf(err, "can't marshal arguments")
+	}
+
+	return info.jobName, data, nil
+}
+
+func (e *Executor) methodID(methodVal reflect.Value) methodID {
+	return methodID{
+		rType: methodVal.Type(),
+		// Hope this will not change in the future versions of go
+		name: runtime.FuncForPC(methodVal.Pointer()).Name(),
+	}
+}
+
+func (e *Executor) validateAndGetMethodInfo(
+	jobName string,
+	receiver reflect.Type,
+	method reflect.Type,
+) (methodInfo, error) {
+	if method.Kind() != reflect.Func {
+		return methodInfo{}, errors.Errorf("func expected, received %s", method)
+	}
+	if method.IsVariadic() {
+		// TODO: support in the future?
+		return methodInfo{}, errors.Errorf("variadic methods not supported %s", method)
+	}
+	if method.NumIn() < 1 {
+		return methodInfo{}, errors.Errorf("is not a method (there is no receiver) %s", method)
+	}
+	if method.In(0) != receiver {
+		return methodInfo{}, errors.Errorf("wrong receiver: %s expected, received %s", method.In(0), receiver)
+	}
+
+	receivesContext := false
+	returnsError := false
+	marshalFields := make([]reflect.StructField, 0, 2*method.NumIn()) // nolint:gomnd // param encoded with 2 fields
+	paramTypes := make([]string, 0, method.NumIn())
+	offset := 1
+
+	for i := 1; i < method.NumIn(); i++ { // 1 is to skip receiver
+		aType := method.In(i)
+
+		if i == 1 && aType == reflect.TypeOf(new(context.Context)).Elem() {
+			receivesContext = true
+			offset++
+			continue
+		}
+
+		idx := i - offset
+
+		paramTypeString, err := e.validateAndGetParameterTypeString(aType)
+		if err != nil {
+			return methodInfo{}, errors.Wrapf(err, "args[%d]", idx)
+		}
+
+		marshalFields = append(marshalFields, reflect.StructField{
+			Name: fmt.Sprintf("Arg%d", idx),
+			Type: aType,
+			Tag:  reflect.StructTag(fmt.Sprintf(`json:"arg%d"`, idx)),
+		})
+		marshalFields = append(marshalFields, reflect.StructField{
+			Name: fmt.Sprintf("Type%d", idx),
+			Type: reflect.TypeOf(""),
+			Tag:  reflect.StructTag(fmt.Sprintf(`json:"type%d"`, idx)),
+		})
+		paramTypes = append(paramTypes, paramTypeString)
+	}
+
+	if method.NumOut() > 0 {
+		if !method.Out(0).Implements(reflect.TypeOf(new(error)).Elem()) {
+			return methodInfo{}, errors.New("if method returns something, it should implement error interface")
+		}
+		if method.NumOut() > 1 {
+			return methodInfo{}, errors.Errorf("method should return at most 1 value, but returns %d", method.NumOut())
+		}
+		returnsError = true
+	}
+
+	return methodInfo{
+		jobName:           jobName,
+		receivesContext:   receivesContext,
+		returnsError:      returnsError,
+		marshalStructType: reflect.StructOf(marshalFields),
+		paramTypes:        paramTypes,
+	}, nil
+}
+
+func (e *Executor) validateAndGetParameterTypeString(aType reflect.Type) (string, error) {
+	switch aType.Kind() {
+	// basic types
+
+	case reflect.String:
+		fallthrough
+	case reflect.Bool:
+		fallthrough
+	case reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64:
+		fallthrough
+	case reflect.Uint, reflect.Uint8, reflect.Uint16, reflect.Uint32, reflect.Uint64:
+		fallthrough
+	case reflect.Float32, reflect.Float64:
+		fallthrough
+	case reflect.Complex64, reflect.Complex128:
+		return aType.Kind().String(), nil
+
+	// structs
+
+	case reflect.Struct:
+		switch aType {
+		case reflect.TypeOf(time.Time{}):
+			return "builtin time.Time", nil
+		case reflect.TypeOf(decimal.Decimal{}):
+			return "github.com/shopspring/decimal.Decimal", nil
+		default:
+			if aType.NumField() == 0 {
+				return "empty struct", nil
+			}
+			// TODO: add more allowed types
+			return "", errors.Errorf("unsupported type %s", aType)
+		}
+
+	// types that may be supported in the future
+
+	case reflect.Slice, reflect.Array, reflect.Map, reflect.Interface, reflect.Ptr:
+		return "", errors.Errorf("unsupported type %s", aType)
+
+	// unsupported
+
+	case reflect.Chan, reflect.Func, reflect.Invalid, reflect.Uintptr, reflect.UnsafePointer:
+		return "", errors.Errorf("unsupported type %s", aType)
+	default:
+		return "", errors.Errorf("[bug] unsupported type %s", aType)
+	}
+}
---
+++ async_test.go
@@ -0,0 +1,275 @@
+package async_test
+
+import (
+	"context"
+	"errors"
+	"reflect"
+	"strconv"
+	"strings"
+	"testing"
+	"time"
+
+	"github.com/maratori/async"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestExecutor(t *testing.T) {
+	t.Parallel()
+	t.Run("executor is error-prone (is it possible to fix?)", func(t *testing.T) {
+		t.Parallel()
+		t.Run("using iota is dangerous", func(t *testing.T) {
+			t.Parallel()
+			domainService := NewDomainService()
+			executor := async.NewExecutor()
+			_, err := executor.Register("job", domainService, DomainService.Struct__MyEnum__Nothing)
+			require.NoError(t, err)
+			// Under the hood MyEnumB is converted to `2` and is stored for the future execution.
+			// If someone adds/removes enum const before MyEnumB, it changes numbering (actual values of enums).
+			// It means that once job enqueued with argument `2` (meaning MyEnumB) will be executed with argument 2.
+			// But now it means not MyEnumB, but some different enum const.
+			_, args, err := executor.Prepare(DomainService.Struct__MyEnum__Nothing, MyEnumB)
+			require.NoError(t, err)
+			assert.JSONEq(t, `{"arg0":2,"type0":"int"}`, string(args))
+		})
+	})
+	t.Run("positive", func(t *testing.T) {
+		t.Parallel()
+		t.Run("register and use two method of same type", func(t *testing.T) {
+			t.Parallel()
+			// These two methods have the same type (signature),
+			// but we get method name from runtime to distinguish between them
+			require.Equal(t,
+				reflect.TypeOf(DomainService.Struct__String__Nothing),
+				reflect.TypeOf(DomainService.OneMoreMethodSameSignature_Struct__String__Nothing),
+			)
+			const arg = "xyz"
+			ctx := context.Background()
+			executor := async.NewExecutor()
+			domainService := NewDomainService()
+			handler1, err := executor.Register("job1", domainService, DomainService.Struct__String__Nothing)
+			require.NoError(t, err)
+			handler2, err := executor.Register("job2", domainService, DomainService.OneMoreMethodSameSignature_Struct__String__Nothing) // nolint:lll
+			require.NoError(t, err)
+			name1, data1, err := executor.Prepare(DomainService.Struct__String__Nothing, arg)
+			require.NoError(t, err)
+			name2, data2, err := executor.Prepare(DomainService.OneMoreMethodSameSignature_Struct__String__Nothing, arg)
+			require.NoError(t, err)
+			assert.Equal(t, "job1", name1)
+			assert.Equal(t, "job2", name2)
+			assert.Equal(t, data1, data2)
+			assert.NotPanics(t, func() {
+				assert.NoError(t, handler1(ctx, data1))
+			})
+			assert.PanicsWithValue(t, arg, func() {
+				_ = handler2(ctx, data2)
+			})
+			requireSingleElemInChan(t, arg, domainService.ch)
+		})
+		registerAndCall := func(t *testing.T, receiver interface{}, method interface{}, args ...interface{}) error {
+			ctx := context.Background()
+			executor := async.NewExecutor()
+			handler, err := executor.Register("job name", receiver, method)
+			require.NoError(t, err)
+			name, data, err := executor.Prepare(method, args...)
+			require.NoError(t, err)
+			require.Equal(t, "job name", name)
+			return handler(ctx, data)
+		}
+		t.Run("Struct__Nothing__Nothing", func(t *testing.T) {
+			t.Parallel()
+			domainService := NewDomainService()
+			err := registerAndCall(t, domainService, DomainService.Struct__Nothing__Nothing)
+			assert.NoError(t, err)
+			requireSingleElemInChan(t, executed, domainService.ch)
+		})
+		t.Run("Struct__String__Nothing", func(t *testing.T) {
+			t.Parallel()
+			domainService := NewDomainService()
+			err := registerAndCall(t, domainService, DomainService.Struct__String__Nothing, "abc")
+			assert.NoError(t, err)
+			requireSingleElemInChan(t, "abc", domainService.ch)
+		})
+		t.Run("Ptr__String__Nothing", func(t *testing.T) {
+			t.Parallel()
+			domainService := NewDomainService()
+			err := registerAndCall(t, &domainService, (*DomainService).Ptr__String__Nothing, "abc")
+			assert.NoError(t, err)
+			requireSingleElemInChan(t, "abc", domainService.ch)
+		})
+		t.Run("Struct__MyString__Nothing", func(t *testing.T) {
+			t.Parallel()
+			domainService := NewDomainService()
+			err := registerAndCall(t, domainService, DomainService.Struct__MyString__Nothing, MyString("abc"))
+			assert.NoError(t, err)
+			requireSingleElemInChan(t, "abc", domainService.ch)
+		})
+		t.Run("Struct__Context_String__Nothing", func(t *testing.T) {
+			t.Parallel()
+			domainService := NewDomainService()
+			err := registerAndCall(t, domainService, DomainService.Struct__Context_String__Nothing, "abc")
+			assert.NoError(t, err)
+			requireSingleElemInChan(t, "abc", domainService.ch)
+		})
+		t.Run("Struct__Context_String__Error - no error", func(t *testing.T) {
+			t.Parallel()
+			domainService := NewDomainService()
+			err := registerAndCall(t, domainService, DomainService.Struct__Context_String__Error, "abc")
+			assert.NoError(t, err)
+			requireSingleElemInChan(t, "abc", domainService.ch)
+		})
+		t.Run("Struct__Context_String__Error - error", func(t *testing.T) {
+			t.Parallel()
+			domainService := NewDomainService()
+			err := registerAndCall(t, domainService, DomainService.Struct__Context_String__Error, errorPrefix+"abc")
+			assert.EqualError(t, err, "abc")
+			requireChanIsEmpty(t, domainService.ch)
+		})
+		t.Run("Struct__Context_String__MyError - no error", func(t *testing.T) {
+			t.Parallel()
+			domainService := NewDomainService()
+			err := registerAndCall(t, domainService, DomainService.Struct__Context_String__MyError, "abc")
+			assert.NoError(t, err)
+			requireSingleElemInChan(t, "abc", domainService.ch)
+		})
+		t.Run("Struct__Context_String__MyError - error", func(t *testing.T) {
+			t.Parallel()
+			domainService := NewDomainService()
+			err := registerAndCall(t, domainService, DomainService.Struct__Context_String__MyError, errorPrefix+"abc")
+			assert.EqualError(t, err, "abc")
+			assert.IsType(t, new(MyError), err)
+			requireChanIsEmpty(t, domainService.ch)
+		})
+		t.Run("Struct__MyEmptyStruct_String__Nothing", func(t *testing.T) {
+			t.Parallel()
+			domainService := NewDomainService()
+			err := registerAndCall(t, domainService, DomainService.Struct__MyEmptyStruct_String__Nothing, MyEmptyStruct{}, "abc")
+			assert.NoError(t, err)
+			requireSingleElemInChan(t, "abc", domainService.ch)
+		})
+	})
+	t.Run("negative", func(t *testing.T) {
+		t.Parallel()
+		t.Run("nil arg (may be allowed in the future)", func(t *testing.T) {
+			t.Parallel()
+			domainService := NewDomainService()
+			executor := async.NewExecutor()
+			_, err := executor.Register("job", domainService, DomainService.Struct__String__Nothing)
+			require.NoError(t, err)
+			_, _, err = executor.Prepare(DomainService.Struct__String__Nothing, nil)
+			assert.EqualError(t, err, "args[0]: expected string, received %!s(<nil>)")
+			requireChanIsEmpty(t, domainService.ch)
+		})
+		t.Run("string instead of MyString (may be allowed in the future)", func(t *testing.T) {
+			t.Parallel()
+			domainService := NewDomainService()
+			executor := async.NewExecutor()
+			_, err := executor.Register("job", domainService, DomainService.Struct__MyString__Nothing)
+			require.NoError(t, err)
+			_, _, err = executor.Prepare(DomainService.Struct__MyString__Nothing, "abc")
+			assert.EqualError(t, err, "args[0]: expected async_test.MyString, received string")
+			requireChanIsEmpty(t, domainService.ch)
+		})
+	})
+}
+
+type DomainService struct {
+	ch chan string
+}
+
+func NewDomainService() DomainService {
+	return DomainService{
+		ch: make(chan string, 10),
+	}
+}
+
+func (d DomainService) Struct__Nothing__Nothing() {
+	d.ch <- executed
+}
+
+func (d DomainService) Struct__String__Nothing(s string) {
+	d.ch <- s
+}
+
+func (d DomainService) OneMoreMethodSameSignature_Struct__String__Nothing(s string) {
+	panic(s)
+}
+
+func (d *DomainService) Ptr__String__Nothing(s string) {
+	d.ch <- s
+}
+
+func (d DomainService) Struct__MyString__Nothing(s MyString) {
+	d.ch <- string(s)
+}
+
+func (d DomainService) Struct__Context_String__Nothing(_ context.Context, s string) {
+	d.ch <- s
+}
+
+func (d DomainService) Struct__Context_String__Error(_ context.Context, s string) error {
+	if strings.HasPrefix(s, errorPrefix) {
+		return errors.New(strings.TrimPrefix(s, errorPrefix))
+	}
+	d.ch <- s
+	return nil
+}
+
+func (d DomainService) Struct__Context_String__MyError(_ context.Context, s string) *MyError {
+	if strings.HasPrefix(s, errorPrefix) {
+		e := strings.TrimPrefix(s, errorPrefix)
+		return (*MyError)(&e)
+	}
+	d.ch <- s
+	return nil
+}
+
+func (d DomainService) Struct__MyEnum__Nothing(e MyEnum) {
+	d.ch <- strconv.Itoa(int(e))
+}
+
+func (d DomainService) Struct__MyEmptyStruct_String__Nothing(_ MyEmptyStruct, s string) {
+	d.ch <- s
+}
+
+const executed = "executed"
+const errorPrefix = "error prefix: "
+
+type MyString string
+
+type MyEnum int
+
+const (
+	MyEnumA MyEnum = iota + 1
+	MyEnumB
+	MyEnumC
+)
+
+type MyEmptyStruct struct{}
+
+type MyError string
+
+func (e *MyError) Error() string {
+	if e == nil {
+		return ""
+	}
+	return string(*e)
+}
+
+func requireSingleElemInChan(t *testing.T, expected string, ch <-chan string) {
+	select {
+	case <-time.After(1 * time.Second):
+		require.Fail(t, "timeout")
+	case actual := <-ch:
+		require.Equal(t, expected, actual)
+	}
+	requireChanIsEmpty(t, ch)
+}
+
+func requireChanIsEmpty(t *testing.T, ch <-chan string) {
+	select {
+	case <-time.After(1 * time.Second):
+	case elem := <-ch:
+		require.Failf(t, "String channel is not empty", "%q received", elem)
+	}
+}
